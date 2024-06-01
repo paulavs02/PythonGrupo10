6707f43f74ae57e608e327fca1ee90c96b9eae23
Laly <lauraedure@gmail.com>
2024-06-01 19:33:22 -0300

    actualizar

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleCaptions'), {
+    const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleCaptions'), {
         interval: 3000, // Cambiar de imagen cada 3 segundos
         ride: 'carousel'
     });

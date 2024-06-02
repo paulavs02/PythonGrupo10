b78ced1bcc9bf6aca8734faec63b8259dd5f912e
Laly <lauraedure@gmail.com>
2024-06-01 23:33:04 -0300

    torneo e index

--- js/javascript.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const carousel = new bootstrap.Carousel(document.querySelector('#carouselExampleCaptions'), {
-        interval: 3000, // Cambiar de imagen cada 3 segundos
-        ride: 'carousel'
-    });
-});
-
